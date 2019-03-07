9597eed159b7bea714ff0e7daa5cccc319a11340
Shaimaa-Ali-Sheridan <shaimaa.ali1@sheridancollege.ca>
2019-03-07 18:39:49 -0500

    Initial Commit

---
+++ src/coursecheckingsystem/CourseChecker.java
@@ -0,0 +1,111 @@
+package coursecheckingsystem;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author shaim
+ */
+public class CourseChecker {
+    public static String[] GRADE_CATEGORY = {"Exams", "Assignments"};
+    public static int EXAMS_WIEGHT = 40;
+    public static int ASSIGNMENTS_WIEGHT = 60;
+    
+    private String studentName;
+    private int    studentID;
+    
+    private String courseName;
+    private String courseCode;
+    
+    private double examsTotalGrade;
+    private double assignmentsTotalGrade;
+
+    /**
+     * @return the studentName
+     */
+    public String getStudentName() {
+        return studentName;
+    }
+
+    /**
+     * @param studentName the studentName to set
+     */
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    /**
+     * @return the studentID
+     */
+    public int getStudentID() {
+        return studentID;
+    }
+
+    /**
+     * @param studentID the studentID to set
+     */
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+
+    /**
+     * @return the courseName
+     */
+    public String getCourseName() {
+        return courseName;
+    }
+
+    /**
+     * @param courseName the courseName to set
+     */
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    /**
+     * @return the courseCode
+     */
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    /**
+     * @param courseCode the courseCode to set
+     */
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    /**
+     * @return the examsTotalGrade
+     */
+    public double getExamsTotalGrade() {
+        return examsTotalGrade;
+    }
+
+    /**
+     * @param examsTotalGrade the examsTotalGrade to set
+     */
+    public void setExamsTotalGrade(double examsTotalGrade) {
+        this.examsTotalGrade = examsTotalGrade;
+    }
+
+    /**
+     * @return the assignmentsTotalGrade
+     */
+    public double getAssignmentsTotalGrade() {
+        return assignmentsTotalGrade;
+    }
+
+    /**
+     * @param assignmentsTotalGrade the assignmentsTotalGrade to set
+     */
+    public void setAssignmentsTotalGrade(double assignmentsTotalGrade) {
+        this.assignmentsTotalGrade = assignmentsTotalGrade;
+    }
+    
+}
---
+++ src/coursecheckingsystem/CourseCheckingSystem.java
@@ -0,0 +1,61 @@
+
+package coursecheckingsystem;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author shaim
+ */
+public class CourseCheckingSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        CourseChecker cc = new CourseChecker();
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Please enter the Student name");
+        String studentName = sc.nextLine();
+        cc.setStudentName(studentName);
+
+        System.out.println("Please enter the Student ID");
+        String studentID = sc.nextLine();
+        cc.setStudentName(studentID);
+        
+        System.out.println("Please enter the course name");
+        String courseName = sc.nextLine();
+        cc.setCourseName(courseName);
+
+        System.out.println("Please enter the course code");
+        String courseCode = sc.nextLine();
+        cc.setCourseCode(courseCode);
+
+        for (int i=0;i<CourseChecker.GRADE_CATEGORY.length;i++)
+        {
+            System.out.println("Enter the total grade for " + CourseChecker.GRADE_CATEGORY[i]);
+            double grade = sc.nextDouble();
+            if (i == 0)
+                cc.setExamsTotalGrade(grade);
+            else
+            {
+                if (i == 1)
+                    cc.setAssignmentsTotalGrade(grade);
+            }
+        }
+        
+        // The student would pass if they get at least 50% is the assignments and 50% in the exams
+        double examsPercentage = cc.getExamsTotalGrade() / CourseChecker.EXAMS_WIEGHT * 100;
+        System.out.println("Percentage of the exams is:"+  examsPercentage);
+        
+        double assignmentsPercentage = cc.getAssignmentsTotalGrade() / CourseChecker.ASSIGNMENTS_WIEGHT * 100;
+        System.out.println("Percentage of the assignments is:"+  assignmentsPercentage);
+    
+        if (examsPercentage >= 50 && assignmentsPercentage >= 50)
+            System.out.println("Pass");
+        else
+            System.out.println("Fail");
+    }
+    
+}
